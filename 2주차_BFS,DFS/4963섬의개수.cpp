df50faedf53dd654417689df5943e3b102e39b14
이찬진 <water0641@naver.com>
2022-03-25 23:45:38 +0900

    [BOJ]섬의개수/실버2

--- 2주차_BFS,DFS/4963섬의개수.cpp
+++ 2주차_BFS,DFS/4963섬의개수.cpp
@@ -1,7 +1,8 @@
 /*
 
- 백준 2606 문제
- 바이러스
+ 백준 4963 문제
+ 섬의 개수
+ 행x 렬y 주의하기
 */
 
 #include <algorithm>
@@ -9,41 +10,72 @@
 #include <vector>
 using namespace std;
 
-vector<int> v[101];
-bool ch[101] = {
+int v[51][51];
+// 입력정보
+bool ch[51][51] = {
     false,
 };
-int cnt = 0;
+//체크 정보
 
-void DFS(int k) {
+//세방향 탐색 오른쪽 ,아래 ,대각선오른쪽 아래
+int dx[8] = {1, 1, 1, 0, -1, -1, -1, 0};
+int dy[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+int w, h;
+void DFS(int i, int k) {
 
-    for (int i = 0; i < v[k].size(); i++) {
-        ch[k] = true;
-        // printf("(%d -> %d)", k, v[k][i]);
+    for (int d = 0; d < 8; d++) {
+        int x = i + dx[d];
+        int y = k + dy[d];
+        // printf("allx %d y %d\n", x, y);
 
-        if (ch[v[k][i]] == false) {
-            cnt++;
-            DFS(v[k][i]);
+        if (x > h || y > w || x < 1 || y < 1) {
+            // 영역을 벗어나면
+            continue;
+        }
+        if (v[x][y] == 1 && ch[x][y] == false) {
+
+            ch[x][y] = true;
+            DFS(x, y);
         }
     }
 }
 
 int main(int argc, char *argv[]) {
-    int n, k;
 
-    scanf("%d", &n);
-    scanf("%d", &k);
-    for (int i = 0; i < k; i++) {
-        int v1, v2;
-        scanf("%d %d", &v1, &v2);
-        //양방향
-        v[v1].push_back(v2);
-        v[v2].push_back(v1);
+    while (1) {
+        scanf("%d %d", &w, &h);
+
+        int cnt = 0;
+
+        if (w == 0 && h == 0) {
+            break;
+        }
+
+        //배열 초기화
+        for (int i = 1; i <= 50; i++) {
+            for (int k = 1; k <= 50; k++) {
+                ch[i][k] = false;
+                v[i][k] = 0;
+            }
+        }
+        // h 행 w 열
+        for (int i = 1; i <= h; i++) {
+            for (int k = 1; k <= w; k++) {
+                scanf("%d", &v[i][k]);
+            }
+        }
+
+        for (int i = 1; i <= h; i++) {
+            for (int k = 1; k <= w; k++) {
+
+                if (ch[i][k] == false && v[i][k] == 1) {
+
+                    DFS(i, k);
+                    cnt++;
+                }
+            }
+        }
+
+        printf("%d\n", cnt);
     }
-    // input
-
-    DFS(1);
-
-    // output
-    printf("%d", cnt);
 }
